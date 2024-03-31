bdf923c8e3bf435cd2258e18e7e006d9506c731d
jaykant02 <jaykanty02@gmail.com>
2024-03-31 23:42:08 +0530

    Back Button Created and Background made

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:playflix/colors.dart';
+import '../constants.dart';
 import '../models/movie.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -9,7 +12,50 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(slivers: [
+        SliverAppBar.large(
+          leading: Container(height: 70, width: 70, margin: const
+            EdgeInsets.only(top: 16,
+            left: 16
+          ),
+            decoration: BoxDecoration(
+                color: Colours.scaffoldBgColor,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_rounded),
+            ),
+            ),
+          backgroundColor: Colours.scaffoldBgColor,
+          expandedHeight: 500,
+          pinned: true,
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(movie.title,style: GoogleFonts.belleza(
+              fontSize: 17,
+              fontWeight : FontWeight.w600,
+              ),
+            ),
+            background: ClipRRect(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(24),
+                bottomRight: Radius.circular(24),
+              ),
+              child: Image.network(
+                '${Constants.imagePath}${movie.backDropPath}',
+                filterQuality: FilterQuality.high,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          ),
+      ],
+    ),
+    );
   }
 
 }
