5485ba5f8023f5f0c8adf6ffe91ad220be6c1c19
jaykant02 <jaykanty02@gmail.com>
2024-03-31 15:56:42 +0530

    Upcoming Movies Created in HomeScreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -59,7 +59,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               const SizedBox(height: 32),
               Text(
-                'Top rated movies',
+                'Top Rated Movies',
               style: GoogleFonts.aBeeZee(fontSize: 25),
               ),
               const SizedBox(height: 32),
@@ -81,7 +81,34 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     );
                   }
-              ),)
+              ),
+              ),
+              const SizedBox(height: 32),
+              Text(
+                'Upcoming Movies',
+                style: GoogleFonts.aBeeZee(fontSize: 25),
+              ),
+              const SizedBox(height: 32),
+              SizedBox(height: 200, width: double.infinity,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Container(
+                            color: Colors.amber,
+                            height: 200,
+                            width: 150,
+                          ),
+                        ),
+                      );
+                    }
+                ),
+              ),
             ],
           ),
         ),
