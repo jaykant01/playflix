87a4d3953afef974bde9d44df9f88dfa794dba2d
jaykant02 <jaykanty02@gmail.com>
2024-03-31 16:29:45 +0530

    Refactor MoviesSlider into Widgets folder

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:playflix/widgets/movies_slider.dart';
 import 'package:playflix/widgets/trending_slider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -40,26 +41,7 @@ class _HomeScreenState extends State<HomeScreen> {
               style: GoogleFonts.aBeeZee(fontSize: 25),
               ),
               const SizedBox(height: 32),
-              SizedBox(height: 200, width: double.infinity,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Container(
-                          color: Colors.amber,
-                        height: 200,
-                        width: 150,
-                        ),
-                      ),
-                    );
-                  }
-              ),
-              ),
+              const MoviesSlider(),
               const SizedBox(height: 32),
               Text(
                 'Upcoming Movies',
@@ -93,3 +75,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
