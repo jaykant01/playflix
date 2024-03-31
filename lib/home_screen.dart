137ccd12caf59d2f14de08219c7dcf02df3b4fa6
jaykant02 <jaykanty02@gmail.com>
2024-03-31 16:22:20 +0530

    Refactor TrendingSlider into Widgets folder

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,6 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:playflix/widgets/trending_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -33,30 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
               Text('Trending Movies', style: GoogleFonts.aBeeZee(fontSize: 25),
               ),
               const SizedBox(height: 32,),
-              SizedBox(
-                width: double.infinity,
-                child: CarouselSlider.builder(
-                    itemCount: 10,
-                    options: CarouselOptions(
-                      height: 300,autoPlay: true,
-                      viewportFraction: 0.55,
-                      enlargeCenterPage: true,
-                      pageSnapping: true,
-                      autoPlayCurve: Curves.fastOutSlowIn,
-                      autoPlayAnimationDuration: const Duration(seconds: 1),
-                    ),
-                    itemBuilder: (context, itemIndex, pageViewIndex){
-                      return ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: Container(
-                          height: 300,
-                          width: 200,
-                          color: Colors.amber,
-                        ),
-                      );
-                    },
-              ),
-              ),
+              const TrendingSlider(),
               const SizedBox(height: 32),
               Text(
                 'Top Rated Movies',
