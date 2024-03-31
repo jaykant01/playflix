95fcd9e078b28863d8cd7d88cf69629f3cac682e
jaykant02 <jaykanty02@gmail.com>
2024-03-31 16:32:35 +0530

    Refactor, call the MoviesSlider for Upcoming movies

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -48,26 +48,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: GoogleFonts.aBeeZee(fontSize: 25),
               ),
               const SizedBox(height: 32),
-              SizedBox(height: 200, width: double.infinity,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: 10,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Container(
-                            color: Colors.amber,
-                            height: 200,
-                            width: 150,
-                          ),
-                        ),
-                      );
-                    }
-                ),
-              ),
+              const MoviesSlider(),
             ],
           ),
         ),
